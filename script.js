eceb0a5ede6746497eef9538f5b2d2057c4a4385
Wandonium <hillarywando@gmail.com>
2019-03-16 12:03:27 +0300

    revert to none-npm version

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var _ = require('lodash');
-
-var array = [1,2,3,4,5,6,7,8];
-console.log("answer:", _.without(array, 3));
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
@@ -15,18 +11,20 @@ r1.addEventListener("click", function() {
 	color2.value = c2;
 	body.style.background = "linear-gradient(to right, "
 	+ c1
-	+ ", " 
+	+ ", "
 	+ c2
 	+ ")";
 
-	css.textContent = body.style.background + ";";	
+	css.textContent = body.style.background + ";";
 })
+
+css.textContent = body.style.backgroundColor + "CSS code goes here...";
 
 function setGradient() {
 	body.style.background = "linear-gradient(to right, "
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
+	+ color1.value
+	+ ", "
+	+ color2.value
 	+ ")";
 
 	css.textContent = body.style.background + ";";
@@ -44,4 +42,3 @@ function getRandomColor() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
-
